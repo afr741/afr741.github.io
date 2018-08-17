f98c2da5637f8eef6c8b111f0aa223daefc01570
afr741 <afr741@mun.ca>
2018-08-17 14:16:45 -0230

    initia commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
+
+
