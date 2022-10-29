2777576e213df9d53eaa69e79acb7c7f55cf7e13
arakhmatov <arakhmatov@paypal.com>
2022-10-29 11:37:31 -0300

    ccm preview

--- README.md
+++
@@ -1,6 +0,0 @@
-# https://afr741.github.io  | https://anoosher.com
-## A web portfolio containing a short intro, list of my works and projects and a contact form.
-
-## Technologies used:  HTML,CSS/SASS, JAVASCRIPT, BOOTSTRAP AND PHP.
-
-## Hosted on AWS EC2 instance.
---
+++ build/precache-manifest.9985eebc5ad36f8d52c0c8f60b149c88.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ec4d561fc47a501b2068d18deb92dd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "13586dedd341f60defe8",
+    "url": "/react-gh-pages/static/css/2.f8a42529.chunk.css"
+  },
+  {
+    "revision": "13586dedd341f60defe8",
+    "url": "/react-gh-pages/static/js/2.61f7542e.chunk.js"
+  },
+  {
+    "revision": "33de2e7c2d7602fc5be2775558ee4bf7",
+    "url": "/react-gh-pages/static/js/2.61f7542e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f568b2d39f7be083b6b",
+    "url": "/react-gh-pages/static/js/main.2676622a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-gh-pages/static/js/main.2676622a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1418f19505e04ac920b8",
+    "url": "/react-gh-pages/static/js/runtime-main.ff5c75c0.js"
+  },
+  {
+    "revision": "697917c2d58876770773d39cbe115b67",
+    "url": "/react-gh-pages/static/media/1.697917c2.png"
+  },
+  {
+    "revision": "c382b5c11cd0622d11758dd58b8520af",
+    "url": "/react-gh-pages/static/media/2.c382b5c1.png"
+  },
+  {
+    "revision": "4cea7b72f3a60ebe41292387cbd2f4bf",
+    "url": "/react-gh-pages/static/media/360-arrow-active.4cea7b72.svg"
+  },
+  {
+    "revision": "9c7bb2e4fc7832a9986a1e004033173a",
+    "url": "/react-gh-pages/static/media/360-arrow.9c7bb2e4.svg"
+  },
+  {
+    "revision": "d807a6c2bd4eda5d831081d947570a64",
+    "url": "/react-gh-pages/static/media/360.d807a6c2.svg"
+  },
+  {
+    "revision": "8466c2b2154745601cf904c0fa2c22c5",
+    "url": "/react-gh-pages/static/media/ChakraPetch-Bold.8466c2b2.woff2"
+  },
+  {
+    "revision": "8bc53b8398436d68e66e1ce6b1d9af2b",
+    "url": "/react-gh-pages/static/media/ChakraPetch-Medium.8bc53b83.woff2"
+  },
+  {
+    "revision": "a4c7b4b277d6734247d52df8f1675339",
+    "url": "/react-gh-pages/static/media/ChakraPetch-Regular.a4c7b4b2.woff2"
+  },
+  {
+    "revision": "8237e11f528505e8a67f945b2287b087",
+    "url": "/react-gh-pages/static/media/ChakraPetch-SemiBold.8237e11f.woff2"
+  },
+  {
+    "revision": "abd698c450c97f1c7bf57af5db7176ff",
+    "url": "/react-gh-pages/static/media/Crew Compartment  Seats.abd698c4.jpg"
+  },
+  {
+    "revision": "f39fba6f5dda7feeb33071ea3a1420b6",
+    "url": "/react-gh-pages/static/media/Industry-Medium.f39fba6f.woff"
+  },
+  {
+    "revision": "7e84a40629465e5196422ce07e66e854",
+    "url": "/react-gh-pages/static/media/IndustryDemi.7e84a406.woff"
+  },
+  {
+    "revision": "424d0ed17449592e9a6984dac5e65dcf",
+    "url": "/react-gh-pages/static/media/Nunito-Bold.424d0ed1.woff"
+  },
+  {
+    "revision": "88987c271ae0d708159f8b487b204e36",
+    "url": "/react-gh-pages/static/media/Nunito-Italic.88987c27.woff2"
+  },
+  {
+    "revision": "e43cc13db918af99d55e8e00a2a1fad0",
+    "url": "/react-gh-pages/static/media/Nunito-Regular.e43cc13d.woff2"
+  },
+  {
+    "revision": "e455332a6a2586d380817469e5f7d451",
+    "url": "/react-gh-pages/static/media/Nunito-SemiBold.e455332a.woff"
+  },
+  {
+    "revision": "03c22bb553f3eee5b8cfa86b078d990d",
+    "url": "/react-gh-pages/static/media/Rendezvous Window .03c22bb5.jpg"
+  },
+  {
+    "revision": "ed3bc3de830d08ff6b7429da7797c0c8",
+    "url": "/react-gh-pages/static/media/aft-lockers.ed3bc3de.jpg"
+  },
+  {
+    "revision": "223f556c719fd713e1e9ff96045b254a",
+    "url": "/react-gh-pages/static/media/arrow-next.223f556c.svg"
+  },
+  {
+    "revision": "ffca79fdf13e3b8fb4b2abce69033f01",
+    "url": "/react-gh-pages/static/media/arrow-previous.ffca79fd.svg"
+  },
+  {
+    "revision": "9f2131c2974ff6dcfefe82f5727c466d",
+    "url": "/react-gh-pages/static/media/background.9f2131c2.svg"
+  },
+  {
+    "revision": "2ba55289466d6a2d18c7d76be88e41d1",
+    "url": "/react-gh-pages/static/media/bg.2ba55289.jpg"
+  },
+  {
+    "revision": "9f2f3855e5400eac8c135cbdedd484a4",
+    "url": "/react-gh-pages/static/media/close-button.9f2f3855.svg"
+  },
+  {
+    "revision": "671c5ff82b9c44914308af1b6c3ef976",
+    "url": "/react-gh-pages/static/media/com-seat.671c5ff8.jpg"
+  },
+  {
+    "revision": "7bd9a61f8f1ba09b4d7f957d243470d7",
+    "url": "/react-gh-pages/static/media/compass.7bd9a61f.svg"
+  },
+  {
+    "revision": "602087eaa7a74d7eaf45b0d0befb2cd5",
+    "url": "/react-gh-pages/static/media/dock-tunnel.602087ea.jpg"
+  },
+  {
+    "revision": "2a5a7c8f859e322ec337c7d3a7c6471c",
+    "url": "/react-gh-pages/static/media/dsky.2a5a7c8f.jpg"
+  },
+  {
+    "revision": "639eeb7a4c834bfe752e619144846738",
+    "url": "/react-gh-pages/static/media/epitaph.639eeb7a.jpg"
+  },
+  {
+    "revision": "3a3a2a73293f90115e8a1f128f4e4953",
+    "url": "/react-gh-pages/static/media/eva.3a3a2a73.jpg"
+  },
+  {
+    "revision": "5eeef2e96722b70aeb4064c884985de1",
+    "url": "/react-gh-pages/static/media/forward-comp.5eeef2e9.jpg"
+  },
+  {
+    "revision": "adb137f0fa13eb025c13d13c727fe7fa",
+    "url": "/react-gh-pages/static/media/grab.adb137f0.svg"
+  },
+  {
+    "revision": "3acfddd5c09c90a4f089f8d626aebca1",
+    "url": "/react-gh-pages/static/media/grabbing.3acfddd5.svg"
+  },
+  {
+    "revision": "86cfe72529ffe444724921c59278bca0",
+    "url": "/react-gh-pages/static/media/gradient.86cfe725.png"
+  },
+  {
+    "revision": "d14cdfc4de165e3a92ffed1534dbf2fa",
+    "url": "/react-gh-pages/static/media/graph.d14cdfc4.svg"
+  },
+  {
+    "revision": "00550db1a113836b40ba9df718f0a474",
+    "url": "/react-gh-pages/static/media/guid-and-nav.00550db1.jpg"
+  },
+  {
+    "revision": "e807c2b0db0282ae787c944ba3c8838c",
+    "url": "/react-gh-pages/static/media/heat.e807c2b0.jpg"
+  },
+  {
+    "revision": "317dbaeae59694e961071b24b1640cf9",
+    "url": "/react-gh-pages/static/media/hotspot-minus.317dbaea.svg"
+  },
+  {
+    "revision": "b929010115c2d57dea9a8a85d904b4de",
+    "url": "/react-gh-pages/static/media/hotspot-plus.b9290101.svg"
+  },
+  {
+    "revision": "2ed35bd1b7942db4a97e5e23c4cced5e",
+    "url": "/react-gh-pages/static/media/hotspotB.2ed35bd1.svg"
+  },
+  {
+    "revision": "190ad55018d419f9ab350dc98c1de635",
+    "url": "/react-gh-pages/static/media/hotspotButton.190ad550.svg"
+  },
+  {
+    "revision": "3d5f3c8deece7e7d2f400bf3e943750c",
+    "url": "/react-gh-pages/static/media/hotspotClicked.3d5f3c8d.svg"
+  },
+  {
+    "revision": "747e9c727b7435d2c4f4bfa60d788536",
+    "url": "/react-gh-pages/static/media/hotspotW.747e9c72.svg"
+  },
+  {
+    "revision": "c16ee6f9bba9eb63a7ae24b0482b989c",
+    "url": "/react-gh-pages/static/media/hotspot_bg.c16ee6f9.svg"
+  },
+  {
+    "revision": "0c11134b1b73c6d52326fa63648d4a48",
+    "url": "/react-gh-pages/static/media/hotspot_bg_active.0c11134b.svg"
+  },
+  {
+    "revision": "24e0e26910142bcc8f4866aefff2c739",
+    "url": "/react-gh-pages/static/media/keypad.24e0e269.PNG"
+  },
+  {
+    "revision": "63cd04b74fdaa372a0b86238aa62bb62",
+    "url": "/react-gh-pages/static/media/left.63cd04b7.svg"
+  },
+  {
+    "revision": "5c1121dab1afdf1581bd70217c05c055",
+    "url": "/react-gh-pages/static/media/main-disp.5c1121da.jpg"
+  },
+  {
+    "revision": "511f22b005fa304157ded8d49d80ac3c",
+    "url": "/react-gh-pages/static/media/marking-days.511f22b0.jpg"
+  },
+  {
+    "revision": "0bebc38aed19fc13c324b30ac0b64add",
+    "url": "/react-gh-pages/static/media/nav-notes.0bebc38a.jpg"
+  },
+  {
+    "revision": "f2b76e2d2c79ead25052033f5ab1c6e6",
+    "url": "/react-gh-pages/static/media/no-sound.f2b76e2d.png"
+  },
+  {
+    "revision": "8b8871e482a76d7e9327b02131564af7",
+    "url": "/react-gh-pages/static/media/nunito-v12-latin-600.8b8871e4.woff2"
+  },
+  {
+    "revision": "4507a1361620a66827e217855c9fff24",
+    "url": "/react-gh-pages/static/media/nunito-v12-latin-700italic.4507a136.woff2"
+  },
+  {
+    "revision": "f82c6fb49774f4e4caa570640e164320",
+    "url": "/react-gh-pages/static/media/nunito-v12-latin-900.f82c6fb4.woff2"
+  },
+  {
+    "revision": "be171ee463bbe9a7c67a7fc23fddb5e7",
+    "url": "/react-gh-pages/static/media/oval.be171ee4.svg"
+  },
+  {
+    "revision": "dd9209f8464584258779f4123090e904",
+    "url": "/react-gh-pages/static/media/overlay.dd9209f8.png"
+  },
+  {
+    "revision": "28b11de0071f122de83ef96e53c446af",
+    "url": "/react-gh-pages/static/media/plus.28b11de0.svg"
+  },
+  {
+    "revision": "08f33cab137d610a283e861836838d4e",
+    "url": "/react-gh-pages/static/media/reaction.08f33cab.jpg"
+  },
+  {
+    "revision": "595aa5d8850284df412b35f6f431de21",
+    "url": "/react-gh-pages/static/media/s1-old.595aa5d8.jpg"
+  },
+  {
+    "revision": "a646227cb02fb6b69a420977a37c03f0",
+    "url": "/react-gh-pages/static/media/s1.a646227c.jpg"
+  },
+  {
+    "revision": "4d016e73041d680b53f910c717332a6f",
+    "url": "/react-gh-pages/static/media/s2-old.4d016e73.jpg"
+  },
+  {
+    "revision": "b5410950caacb70d72026982072b2d43",
+    "url": "/react-gh-pages/static/media/s2.b5410950.jpg"
+  },
+  {
+    "revision": "89dfc62b611b0bfdf2ad20a18a54ffe2",
+    "url": "/react-gh-pages/static/media/sextant.89dfc62b.jpg"
+  },
+  {
+    "revision": "5d8d3e1686c0600a981be1912a58604b",
+    "url": "/react-gh-pages/static/media/shuttle.5d8d3e16.png"
+  },
+  {
+    "revision": "f37aa8ca72b5548a255c62dab1614437",
+    "url": "/react-gh-pages/static/media/sprites.f37aa8ca.svg"
+  },
+  {
+    "revision": "a482777df486c4bf4cc3e0a9677cd36b",
+    "url": "/react-gh-pages/static/media/startover.a482777d.svg"
+  },
+  {
+    "revision": "e1f6742dd67c4862620add58b156b659",
+    "url": "/react-gh-pages/static/media/thumbnail_1.e1f6742d.png"
+  },
+  {
+    "revision": "b4756851d6684c1e7d7462f9cf0fbfcf",
+    "url": "/react-gh-pages/static/media/thumbnail_2.b4756851.png"
+  },
+  {
+    "revision": "b00829401fb872f9ee7c84e16d1c1195",
+    "url": "/react-gh-pages/static/media/thumbnail_3.b0082940.png"
+  },
+  {
+    "revision": "f3c4c038af3cc4bbcf610291aee5d66f",
+    "url": "/react-gh-pages/static/media/touch.f3c4c038.svg"
+  },
+  {
+    "revision": "7a3d73972bd8ea500ad77ea61b9f868b",
+    "url": "/react-gh-pages/static/media/wall-lockers.7a3d7397.jpg"
+  },
+  {
+    "revision": "e9a10f4b3eed368d37a79daf421e736a",
+    "url": "/react-gh-pages/static/media/waste.e9a10f4b.jpg"
+  },
+  {
+    "revision": "c9369e329aaa20ffb708855c9e86f1cf",
+    "url": "/react-gh-pages/static/media/window.c9369e32.jpg"
+  }
+]);
